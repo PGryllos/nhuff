8d239e46eb324ea9e8aa112550742c9a94065434
Gryllos Prokopis <gryllosprokopis@gmail.com>
2015-12-13 00:16:50 +0100

    Add huffmandeco implementation myhuffmandeco.m

---
+++ myhuffmandeco.m
@@ -0,0 +1,37 @@
+function dsig = myhuffmandeco(comp,dict) % comp must be a numeric vector (row or col)
+dsig = []
+[n1, n2] = size(comp);
+if n1 > n2
+    n = n1;
+else
+    n = n2;
+end
+dictSize = size(dict,1);
+
+function symbol = getSymbol(code)
+symbol = NaN;
+for j = 1:dictSize
+    tmpCode = cell2mat(dict(j,2));
+    if tmpCode == code && size(tmpCode) == size(code)
+        symbol = cell2mat(dict(j,1));
+        break;
+    end
+end
+end
+
+beginning = 1;
+ending = 1;
+while ending < n
+    symbol = getSymbol(comp(beginning:ending));
+    while isnan(symbol)
+        ending += 1;
+        symbol = getSymbol(comp(beginning:ending));
+    end
+    dsig(size(dsig,2)+1) = symbol;
+    if ending != n
+        ending += 1;
+        beginning = ending;
+    end
+end
+
+end
